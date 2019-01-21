13df802566b2df9cce54aa984dad4bbc4cf3b601
Maciej Pokorski <maciekpokorski@gmail.com>
2019-01-21 19:56:17 +0100

    add flow types

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const sortArr = (A) => {
+const sortArr : Array<number> = (A : Array<number>) => {
 
     return A.sort((a, b) => a - b);
 }
 
-const isPositive = (value) => value > -1
+const isPositive : boolean = (value) => value > -1
 
-const solution = (A) => {
-    let smallestMissingInteger = 1;
+const solution : number = (A : Array<number>) => {
+    let smallestMissingInteger : number = 1;
     
-    const input = sortArr(A);
+    const input : Array<number> = sortArr(A);
 
     for (let index = 0; index < input.length; index++) {
         let currentCheckingValue = input[index];
