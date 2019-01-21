66201a2bcf4f777be19c9aad42b91f9116126220
Maciej Pokorski <maciekpokorski@gmail.com>
2019-01-21 19:52:59 +0100

    extract variable name to code be more readable

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ const sortArr = (A) => {
     return A.sort((a, b) => a - b);
 }
 
+const isPositive = (value) => value > -1
+
 const solution = (A) => {
     let smallestMissingInteger = 1;
     
     const input = sortArr(A);
 
     for (let index = 0; index < input.length; index++) {
-      if(input[index] > -1 && input[index] === smallestMissingInteger){
-          smallestMissingInteger++;
-      }
+        let currentCheckingValue = input[index];
+        if(isPositive(currentCheckingValue) && input[index] === smallestMissingInteger){
+            smallestMissingInteger++;
+        }
     }
 
     return smallestMissingInteger;
